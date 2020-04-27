04823c10c9f46dbfe4aa538061e727ad85f00d46
yagyanshbhatia <yagyansh1999@gmail.com>
2020-04-27 15:28:52 +0530

    data curation script

---
+++ data/curation.py
@@ -0,0 +1,19 @@
+import json
+from pprint import pprint
+from os import listdir
+import os
+from os.path import isfile, join
+
+mypath = '/Users/yagyanshbhatia/code2seq/data/valid'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f[-5:]=='jsonl']
+
+for f in onlyfiles:
+    with open(os.path.join(mypath, f), 'r') as f:
+        sample_file = f.readlines()
+    
+    for sample in sample_file:
+        code = json.loads(sample)['code']
+        print(code)
+        print()
+        print()
+        print()
