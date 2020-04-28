be875195205532cebe8eb8c6373dc6783cc7bf6c
yagyanshbhatia <yagyansh1999@gmail.com>
2020-04-28 12:54:17 +0530

    create seperate files for each method

--- data/curation.py
+++ data/curation.py
@@ -4,16 +4,20 @@ from os import listdir
 import os
 from os.path import isfile, join
 
-mypath = '/Users/yagyanshbhatia/code2seq/data/valid'
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f[-5:]=='jsonl']
+mypathOrig = '/Users/yagyanshbhatia/code2seq/data/'
+dirs = ['test', 'train', 'valid']
 
-for f in onlyfiles:
-    with open(os.path.join(mypath, f), 'r') as f:
-        sample_file = f.readlines()
-    
-    for sample in sample_file:
-        code = json.loads(sample)['code']
-        print(code)
-        print()
-        print()
-        print()
+for dir in dirs:
+    mypath = mypathOrig + dir
+
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f[-5:]=='jsonl']
+
+    for f in onlyfiles:
+        with open(os.path.join(mypath, f), 'r') as f:
+            sample_file = f.readlines()
+        
+        for index, sample in enumerate(sample_file):
+            code = json.loads(sample)['code']
+            filename = dir + '_' + str(index) + '.java'
+            with open(os.path.join(mypath, filename), 'w') as f:
+                f.write(code)
